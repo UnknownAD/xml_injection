8743839fc1ae36366ca5b81e7ddcc190a18bd0a6
UnknownAD <59281997+UnknownAD@users.noreply.github.com>
2020-08-22 16:29:06 -0400

    Add files via upload

---
+++ target.php
@@ -0,0 +1,10 @@
+<?php
+libxml_disable_entity_loader(false);
+$file='<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE login SYSTEM "http://localhost/e.ent">
+<login><user>&name;</user><pass>password</pass></login>';
+$dom=new DOMDocument();
+$dom->loadXML($file,LIBXML_NOENT | LIBXML_DTDLOAD);
+$element=$dom->getElementsByTagName('user')[0];
+echo $element->nodeValue;
+?>
